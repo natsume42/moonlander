bb2e7ff9fd6cf4947aa9fdeb920edbac79220567
natsume <natsume@nowhere.jp>
2021-12-06 19:39:28 +0100

    Bilde OSL-Funktionalität mit einem custom keycode nach. Funktioniert nicht.
    Aktuell erzieltes Verhalten:
    - OSL Layer wird durch "OSL3" keycode kurzeitig aktiviert, aber danach sofort
      wieder deaktiviert (zurück auf Ebene 0 und keine aktive OSL-Ebene mehr).
    - OSL als Modifier funktioniert korrekt.
    
    Weitere gesammelte Erkenntnisse:
    - Wenn clear_oneshot_layer_state(ONESHOT_PRESSED); nicht gerufen wird, dann
      bleibt die OSL-Ebene erwartungsgemäß dauerhaft aktiv.
    - Eine in der OSL-Ebene gedrückte zweite OSL-Taste funktioniert korrekt.
    - Ein vorher gerufenes reset_oneshot_layer verändert das Verhalten
      (erwartungsgemäß) nicht.
    - Callback-Hooks (s. customlogic.h) helfen OSL-Ebenenänderungen zu protokollieren.
    
    Vermutlich löst OSL3 bereits selbst ein ONESHOT_OTHER_KEY_PRESSED aus.
    Der Grund ist aber nicht ersichtlich. Ebenso wenig wie ein Gegenmittel dafür.

--- keyboards/moonlander/keymaps/mine/customlogic.h
+++ keyboards/moonlander/keymaps/mine/customlogic.h
@@ -1,16 +1,16 @@
 /** \brief Change input language and keyboard layer to Japanese input.
-* 
-* ALT+Shift+2 to switch to Japanese IME
-* INT2 (Hiragana key on Japanese keyboard) to switch to Hiragana
-* Switch to JISX6004 layer.
-*/
+ *
+ * ALT+Shift+2 to switch to Japanese IME
+ * INT2 (Hiragana key on Japanese keyboard) to switch to Hiragana
+ * Switch to JISX6004 layer.
+ */
 void switch_to_japanese(void) {
     SEND_STRING(SS_LALT(SS_LSFT(SS_TAP(X_2))) SS_DELAY(100) SS_TAP(X_INT2));
     layer_move(5);
 }
 
 /** \brief Change input language and keyboard layer to German input.
- * 
+ *
  * ALT+Shift+1 to switch to Japanese IME
  * Switch to Mine layer.
  */
@@ -18,3 +18,18 @@ void switch_to_german(void) {
     SEND_STRING(SS_LALT(SS_LSFT(SS_TAP(X_1))));
     layer_move(0);
 }
+
+//void oneshot_layer_changed_user(uint8_t layer) {
+//    if (layer == 3) {
+//        SEND_STRING(SS_TAP(X_3));
+//    }
+//    if (layer == 0) {
+//        SEND_STRING(SS_TAP(X_0));
+//    }
+//    if (layer == 4) {
+//        SEND_STRING(SS_TAP(X_4));
+//    }
+//    if (!layer) {
+//        SEND_STRING(SS_TAP(X_X));
+//    }
+//}

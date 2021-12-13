9917857bf19ff889da690f3969bef3c3d46d0861
natsume <natsume+github@posteo.jp>
2021-12-13 20:48:47 +0100

    Display Num Lock on NumPad layer using {green} led 5.

--- keyboards/moonlander/keymaps/mine/leds.c
+++ keyboards/moonlander/keymaps/mine/leds.c
@@ -2,6 +2,16 @@
 #include "layers.h"
 extern bool mcp23018_leds[3];
 
+bool num_lock = true;
+
+bool led_update_user(led_t state) {
+    if ((1UL << numPadL) <= layer_state && layer_state < (1UL << prgSymL)) {
+        num_lock = state.num_lock;
+        ML_LED_5(num_lock);
+    }
+    return false;
+}
+
 layer_state_t layer_state_set_user(layer_state_t state) {
     ML_LED_1(false);
     ML_LED_2(false);
@@ -21,7 +31,6 @@ layer_state_t layer_state_set_user(layer_state_t state) {
             ML_LED_6(true);
             break;
         case (1UL << qwertzL)...(1UL << editL) - 1 :
-            ML_LED_1(true);
             ML_LED_2(true);
             break;
         case (1UL << editL)...(1UL << numPadL) - 1 :
@@ -29,17 +38,19 @@ layer_state_t layer_state_set_user(layer_state_t state) {
             break;
         case (1UL << numPadL)...(1UL << prgSymL) - 1 :
             ML_LED_4(true);
+            //led_t state = host_keyboard_led_state();
+            ML_LED_5(num_lock);
             break;
         case (1UL << prgSymL)...(1UL << txtSymL) - 1 :
-            ML_LED_2(true);
+            ML_LED_5(true);
             break;
         case (1UL << txtSymL)...(1UL << fncKeysL) - 1:
-            ML_LED_2(true);
             ML_LED_5(true);
+            ML_LED_6(true);
             break;
         case (1UL << fncKeysL)...(1UL << mediaL) - 1:
             ML_LED_4(true);
-            ML_LED_5(true);
+            ML_LED_6(true);
             break;
         case (1UL << mediaL)...(1UL << (mediaL+1)) - 1 :
             ML_LED_4(true);

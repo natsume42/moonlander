4961a132f5281a43963f80f58d424a363bd513fe
natsume <natsume+github@posteo.jp>
2021-12-14 19:01:42 +0100

    Reduce led lights to layout and numPad scenarios.

--- keyboards/moonlander/keymaps/mine/leds.c
+++ keyboards/moonlander/keymaps/mine/leds.c
@@ -24,11 +24,8 @@ layer_state_t layer_state_set_user(layer_state_t state) {
         case (1UL << mineL)...(1UL << ja1L) - 1:
             break;
         case (1UL << ja1L)...(1UL << ja2L) - 1:
-            ML_LED_3(true);
-            break;
         case (1UL << ja2L)...(1UL << qwertzL) - 1:
             ML_LED_3(true);
-            ML_LED_6(true);
             break;
         case (1UL << qwertzL)...(1UL << editL) - 1 :
             ML_LED_2(true);
@@ -37,25 +34,8 @@ layer_state_t layer_state_set_user(layer_state_t state) {
             ML_LED_6(true);
             break;
         case (1UL << numPadL)...(1UL << prgSymL) - 1 :
-            ML_LED_4(true);
-            //led_t state = host_keyboard_led_state();
+            ML_LED_1(true);
             ML_LED_5(num_lock);
-            break;
-        case (1UL << prgSymL)...(1UL << txtSymL) - 1 :
-            ML_LED_5(true);
-            break;
-        case (1UL << txtSymL)...(1UL << fncKeysL) - 1:
-            ML_LED_5(true);
-            ML_LED_6(true);
-            break;
-        case (1UL << fncKeysL)...(1UL << mediaL) - 1:
-            ML_LED_4(true);
-            ML_LED_6(true);
-            break;
-        case (1UL << mediaL)...(1UL << (mediaL+1)) - 1 :
-            ML_LED_4(true);
-            ML_LED_5(true);
-            ML_LED_6(true);
             break;
         default:
             break;

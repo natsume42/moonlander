7b97e5df9b9711ce7cbdfc05bd86682dda5e9e58
natsume <natsume+github@posteo.jp>
2021-12-29 22:07:12 +0100

    Change hue calculation of heatmap to a logarithmic function.

--- keyboards/moonlander/keymaps/mine/rgb_matrix/heatmap.c
+++ keyboards/moonlander/keymaps/mine/rgb_matrix/heatmap.c
@@ -1,12 +1,13 @@
 #include "heatmap.h"
 #include "rgb_matrix_utils.h"
+#include "math.h"
 
 static uint16_t   keycount[DRIVER_LED_TOTAL];
 static bool       disabled_keys[DRIVER_LED_TOTAL];
-static uint16_t   max, min, max2, min2;
+static uint16_t   max, min;
 static bool       heatmap_enabled = false;
 static const char separator[]     = " ";
-static char string[64];
+static char       string[64];
 
 void heatmap_toggle() { heatmap_enabled = !heatmap_enabled; }
 
@@ -16,7 +17,7 @@ void heatmap_reset() {
         disabled_keys[i] = false;
     }
 
-    max = min = max2 = min2 = 0;
+    max = min = 0;
 }
 
 void dump_counts_of_pad(uint16_t* pad) {
@@ -55,13 +56,7 @@ void heatmap_dump() {
     }
 
     itoa(min, string, 10);
-    SEND_STRING("\nmin, min2, max2, max ");
-    SEND_STRING(string);
-    SEND_STRING(separator);
-    itoa(min2, string, 10);
-    SEND_STRING(string);
-    SEND_STRING(separator);
-    itoa(max2, string, 10);
+    SEND_STRING("\nmin, max ");
     SEND_STRING(string);
     SEND_STRING(separator);
     itoa(max, string, 10);
@@ -107,30 +102,21 @@ void handle_overflow(void) {
 }
 
 void update_extremes(void) {
-    max = max2 = 0;
-    min2 = min = UINT16_MAX;
+    max = 0;
+    min = UINT16_MAX;
 
     for (int i = 0; i < DRIVER_LED_TOTAL; i++) {
-        if (keycount[i] < min2) {
-            if (keycount[i] < min) {
-                min2 = min;
-                min  = keycount[i];
-            } else if (min != keycount[i]) {
-                min2 = keycount[i];
-            }
+        if (keycount[i] < min && keycount[i] > 0) {
+            min = keycount[i];
         }
 
-        if (keycount[i] > max2) {
-            if (keycount[i] > max) {
-                max2 = max;
-                max  = keycount[i];
-            } else if (max != keycount[i]) {
-                max2 = keycount[i];
-            }
+        if (keycount[i] > max) {
+            max = keycount[i];
         }
     }
-    if (min2 > max2) {
-        max2 = min2;
+
+    if (min > max) {
+        min = max;
     }
 }
 
@@ -152,39 +138,33 @@ void heatmap_process(keypos_t key) {
 /*
  * Color calculation uses a simple sigmoid function:
  * 0 == count: black.
- * 0 < count < min2: violet.
- * min2 <= count < max2: linear color gradient from violet to red.
- * max2 > count: constant red.
- *
- * min2/max2 are used instead of min/max to cut of extrem outliers like SPACE
- * key, which is used much more frequent than any other key and would therefore
- * cause a "blue shift" of all "regular" keys (letters).
- * From a mathematical point of view using 10%/90% quantile would be a better,
- * choice but their calculation was deemed to be too expensive in terms of
- * resource consumption.
+ * 0 < count < min: violet.
+ * min <= count < max: logarithmic color gradient from violet to red.
  */
 HSV calc_color(int index) {
     const uint8_t violet = 180; /* Used as color for min2 and below. */
     const uint8_t red    = 0;   /* Used for max2 and above. */
 
-    /* Calculate differences first to get numbers in low ranges to avoid rounding errors. */
-    uint16_t diff  = max2 - keycount[index];
-    uint16_t range = max2 - min2;
-    double_t hue   = 0;
+    double_t      hue    = 0;
 
-    /* Avoid division by zero. */
-    if (range == 0 || diff == 0 || keycount[index] >= max2) {
-        hue = red;
-    } else {
-        if (0 < keycount[index] && keycount[index] < min2) {
-            hue = violet;
+    if (keycount[index] > 0) {
+        /* Calculate differences first to get numbers in low ranges to avoid rounding errors. */
+        double diff  = log(max) - log(keycount[index]);
+        double range = log(max) - log(min);
+
+        /* Avoid division by zero. */
+        if (range == 0 || diff == 0) {
+            hue = red;
+        } else {
+            hue = (double)violet * ((double)diff / (double)range) + red;
         }
-        hue = (double)violet * ((double)diff / (double)range) + red;
     }
+
     HSV color = {(int8_t)hue, 255, 255};
     if (keycount[index] == 0 || disabled_keys[index]) {
         color.v = 0;
     }
+
     return color;
 }
 

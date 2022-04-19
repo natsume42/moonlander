475aac5f536f9794d6e21c739cdf7e63e866146f
natsume <natsume+github@posteo.jp>
2022-04-19 14:45:45 +0200

    Fix issue where layer has already been activated and a fast layer tap to this layer is done.
    This lead to the situation that no layer was active after the fast layer tap deactivates the (last) active layer.

--- keyboards/moonlander/keymaps/mine/custom_actions/fast_layer_tap.c
+++ keyboards/moonlander/keymaps/mine/custom_actions/fast_layer_tap.c
@@ -1,7 +1,8 @@
 #include "fast_layer_tap.h"
 
-static uint8_t fast_layer  = 0;
+static uint8_t fast_layer     = 0;
 static bool    layer_switched = false;
+static bool    is_same_layer  = false;
 
 void process_fast_layer_tap(uint8_t layer, uint8_t keycode, keyrecord_t *record) {
     if (record->event.pressed) {
@@ -14,7 +15,10 @@ void process_fast_layer_tap(uint8_t layer, uint8_t keycode, keyrecord_t *record)
         fast_layer = 0;
 
         if (layer_switched) {
-            layer_off(layer);
+            if (!is_same_layer) {
+                layer_off(layer);
+            }
+            is_same_layer  = false;
             layer_switched = false;
         } else {
             if (record->tap.count < 3) {
@@ -30,6 +34,10 @@ bool is_fast_layer_armed(void) { return fast_layer != 0; }
 
 bool fast_layer_switch_when_armed(keyrecord_t *record) {
     if (is_fast_layer_armed() && record->event.pressed && !layer_switched) {
+        if (IS_LAYER_ON(fast_layer)) {
+            is_same_layer = true;
+        }
+
         layer_on(fast_layer);
         layer_switched = true;
         return true;

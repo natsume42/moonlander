80bd523d32078449cf405b262239fab03fbae343
natsume <natsume+github@posteo.jp>
2022-06-10 19:22:20 +0200

    Move mod for switching permanently to fncL.

--- keyboards/moonlander/keymaps/mine/rgb_matrix/rgb_matrix.c
+++ keyboards/moonlander/keymaps/mine/rgb_matrix/rgb_matrix.c
@@ -229,7 +229,7 @@ const uint8_t PROGMEM ledmap[][DRIVER_LED_TOTAL][3] = {
                   BLACK, BLACK, BLACK, BLACK, BLACK,
                   BLACK, BLACK, CYBER_Y, BLACK, BLACK,
                   BLACK, BLACK, CYBER_Y, BLACK, BLACK,
-                  BLACK, BLACK, CYBER_Y, BLACK, BLACK,
+                  BLACK, BLACK, CYBER_Y, WHITE, BLACK,
                   BLACK, BLACK, BLACK, BLACK,
                   FOREST_R, WHITE, BTS,
 
